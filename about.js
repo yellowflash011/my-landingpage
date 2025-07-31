72a061ea23ee56431ed240da470381ab029c7c92
Hussain Nazir Awan <hhussainnazir@gmail.com>
2025-07-31 14:35:16 +0500

    created about html and its styling

---
+++ about.js
@@ -0,0 +1,9 @@
+// Hamburger Menu and Navigation Toggle
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('nav ul');
+
+// Toggle the menu on click
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+  hamburger.classList.toggle('active'); // Optional, to add an active class to the hamburger (e.g., to change the icon)
+});
